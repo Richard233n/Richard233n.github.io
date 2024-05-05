76874a6ade94e5c410f68375ae6f79a0789ed61d
richard233n <1943126663@qq.com>
2024-05-05 18:44:52 +0800

    Site updated: 2024-05-05 18:44:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/05/12/Hexo/Hexo部署/","2024/05/04/游戏相关/森林之子指令分享/","2024/05/04/游戏相关/森林之子Mod分享/","2024/05/05/游戏相关/森林之子物品id/"];function toRandomPost(){
+var posts=["2022/05/12/Hexo/Hexo部署/","2024/05/04/游戏相关/森林之子Mod分享/","2024/05/05/游戏相关/森林之子物品id/","2024/05/04/游戏相关/森林之子指令分享/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
